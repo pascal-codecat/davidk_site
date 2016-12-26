6a18c3b6d4736b10e0c74dfafd8186790ad7d319
pascal-codecat <pascal.zwicker@windowslive.com>
2016-12-26 11:11:17 +0100

    solved issue nav-list

--- js/main.js
+++ js/main.js
@@ -30,13 +30,13 @@ $('.willkommen-bei')
 		.css('-webkit-animation-duration', '2s')
 		.css('-ms-animation-duration', '2s')
 		.addClass('animated fadeIn');
-
-		// $('.willkommen-l').animate({width: '+=2vw'},'fast')
-		// 	.css('background-color','white')
-		// 	.css('font-weight','900')
-		// 	.css('color','#076e7b')
-		// 	.addClass('extended');
 	});
+
+	$('.willkommen-l').animate({width: '+=2vw'},'fast')
+		.css('background-color','white')
+		.css('font-weight','900')
+		.css('color','#076e7b')
+		.addClass('extended');
 
 // END -> ANIMATION
 
